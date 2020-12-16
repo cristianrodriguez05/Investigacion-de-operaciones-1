31dda92f8ffc849b215d3f5cedc5cbf9bd78b9dc
cristianrodriguez05 <30886461+cristianrodriguez05@users.noreply.github.com>
2020-12-16 14:29:13 -0500

    Add files via upload

---
+++ Cuarta entrega/Implementacion ejemplos 1,2 y 3.py
@@ -0,0 +1,35 @@
+def mochila(W, wt, val, n):
+   K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+   #Tabla de abajo hacia arriba
+   for i in range(n + 1):
+      for w in range(W + 1):
+         if i == 0 or w == 0:
+            K[i][w] = 0
+         elif wt[i-1] <= w:
+            K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w])
+         else:
+            K[i][w] = K[i-1][w]
+    # Devuelve el valor máximo de beneficio que puede almacenar la bolsa
+   return K[n][W]
+
+
+val = [100,60,70,15,9]
+wt = [42,23,21,15,7]
+W = 60
+n = len(val)
+print('Solucion ejemplo 1')
+print(mochila(W, wt, val, n))
+
+val = [11000000,9000000,1000000]
+wt = [10000000,6000000,4000000]
+W = 10000000
+n = len(val)
+print('Solucion ejemplo 2')
+print(mochila(W, wt, val, n))
+
+val = [15,8,15,17,7]
+wt = [5000000,2520000,3600000,4700000,1200000]
+W = 5000000
+n = len(val)
+print('Solucion ejemplo 3')
+print(mochila(W, wt, val, n))
